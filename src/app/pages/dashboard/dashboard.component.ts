38369d8cce67fb8f38dffdf9a4c0e3b145e6f7c2
Marcos Oliveira <marcos@techpark-br.com>
2019-03-09 11:39:29 -0300

    Dashboard cards implementation

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../services/user.service';
 
 @Component({
@@ -8,6 +8,53 @@ import {UserService} from '../../services/user.service';
 })
 export class DashboardComponent implements OnInit {
 
+  dashQuestCards: DashQuestCards[] = [
+    {
+      icon: 'forum',
+      iconColor: '#22a7f0',
+      description: 'Tirar uma dúvida sobre algum assunto ou disciplina',
+      tooltip: '',
+      router: '/forum'
+
+    },
+    {
+      icon: 'wb_incandescent',
+      iconColor: '#f7ca18',
+      description: 'Aprender algo novo',
+      tooltip: '',
+      router: '/cursos'
+
+    }
+  ];
+
+  courseDashboardCards: CourseDashboardCards[] = [
+    {
+      img: '../../../assets/img/courses/js-course-bg.jpg',
+      title: 'Javascript Básico',
+      description: '',
+      lessonsAmount: 100,
+      lessonsFinished: 55,
+    },
+    {
+      img: '../../../assets/img/courses/angular-course-bg.jpg',
+      title: 'Angular 7',
+      description: '',
+      lessonsAmount: 126,
+      lessonsFinished: 23,
+    },
+    {
+      img: '../../../assets/img/courses/historia_brasil-course-bg.jpg',
+      title: 'História do Brasil',
+      description: '',
+      lessonsAmount: 25,
+      lessonsFinished: 24,
+    },
+  ];
+
+  getCoursePercentage(amount: number, finished: number) {
+    return Math.floor(100 / amount * finished);
+  }
+
   constructor(
     private user: UserService
   ) { }
@@ -15,4 +62,20 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
   }
 
+}
+
+export interface DashQuestCards {
+  icon: string;
+  iconColor: string;
+  description: string;
+  tooltip: string;
+  router: string;
+}
+
+export interface CourseDashboardCards {
+  img: string;
+  title: string;
+  description: string;
+  lessonsAmount: number;
+  lessonsFinished: number;
 }
